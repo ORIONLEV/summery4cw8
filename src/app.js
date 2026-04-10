57b1915ce4fa3f5bcdf6d626c07b6882227af79f
Lev <lev2010alex@gmail.com>
2026-04-10 23:09:07 +0200

    second

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-import color from './colorpicker.json';
-console.log(color);
+import colors from '../colorpicker.json' with { type: 'json' };
+
+const divRef = document.querySelector(".js-palette");
+
+function opening(arr) {
+  const item = arr.map((elem) => {
+    return `
+      <div class="color-card">
+        <div class="color-swatch" 
+             data-hex="${elem.hex}" 
+             data-rgb="${elem.rgb}" 
+             style="background-color: ${elem.hex}">
+        </div>
+        <div class="color-meta">
+          <p>HEX: ${elem.hex}</p>
+          <p>RGB: ${elem.rgb}</p>
+        </div>
+      </div>
+    `;
+  }).join("");
+
+  divRef.innerHTML = item;
+}
+opening(colors);
+
 
